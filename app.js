80d9d460eedbb6088dc8a5c2da64f360e462d61b
DIEGO FLORES ALCANTAR <diegoedvflores03@gmail.com>
2022-11-06 23:50:23 -0600

    comentando el codigo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 
-//datos
+//primero dividi los temas que queria tener idsponibles en arreglos deobjetos para que fuese mas facil para el usuario decidir un tema
+//cada objeto representa una pregunta del tema, tiene 3 propiedades principales que nor serviran como cuerpo de nuestro objeto
 
 const css = [
     {
@@ -369,7 +370,8 @@ const deporte = [
 ]
 
 
-
+//Creamos una clase que va a estructurar cuando llamemos una pregunta, dicha clase tendra un constructor que va a resivir por parametros las propiedades del objeto(pregunta)
+//tambien esta clase tendra un metodo llamado respuestaCorrecta que resivira por parametro la propiedad opcion que es una posible respuesta y retorna si la opcion elegida es igual a la respuesta
 
 
 
@@ -397,20 +399,27 @@ class Pregunta {
     }
 }
 
+
+//declaramos otra clase que se encargara de mostrar las preguntas del arreglo(tema) elegido
+//al llamar esta clase declaramos dos variables que guardaran el puntaje y la posicion de la pregunta donde se encuentra que servira para verificar que la pregunta ha sido contestada y poder pasar a la siguiente
+//el constructor de esta clase resivira por parametro la propiedad preguntas del tema y la comparara
+//el metodo numeroDePregunta no resive nada por parametro por que retornara la posicion actual de las preguntas del tema
+//el metodo adivina resive el parametro respuesta y condiciona si el metodo respuestaCorrecta es igual al parametro respuesta si es asi la variable puntaje aumenta 1
+//el metodo aFinalizado solo retorna si la longitud de la propiedad preguntas es igual a la variable preguntaActual
 class Juego {
     preguntaActual = 0
     puntaje = 0
 
     /**
-     * 
-     * @param {Pregunta} preguntas 
+     *
+     * @param {Pregunta} preguntas
      */
     constructor(preguntas) {
         this.preguntas = preguntas
     }
 
     /**
-     * 
+     *
      * @returns {Pregunta} retorna la pregunta encontrada
      */
     numeroDePregunta()  {
@@ -419,11 +428,10 @@ class Juego {
 
 
     /**
-     * 
+     *
      * @param {string} respuesta texto de la respuesta elegida
      */
     adivina(respuesta) {
-        console.log(respuesta)
         if (this.numeroDePregunta().respuestaCorrecta(respuesta)) {
             this.puntaje++
         }
@@ -436,6 +444,12 @@ class Juego {
     }
 }
 
+
+//creamos esta clase para estructurar la interaccion del usuario en el juego
+//su constructor esta vacio por que solo necesitaremos los metodos
+//el metodo mostrarPregunta nos permite resivir la pregunta a mostrar al usuario y guardarla en una variable cuya funcion sera pintar la pregunta en un h2 con un id
+//el metodo mostrarOpciones nos permite crear un boton dependiendo el numero de respuestas que tenga nuestra pregunta y agregar este boton a un contenedor previamente creado en html con un id, el parametro o funcion callback nos sirve para pasar el texto actual que tenga la posible respuesta al boton
+//el metodo mostrar puntuacion resive por parametro la variable puntaje y ejecuta una variable donde pinta un h2 y concatena el valor de la variable puntaje, luego crea otra variable que guarda todo el juego para despues pintar solo la puntuacion en el index con la propiedad innerHTML
 class Funcionalidad {
 
     constructor(){}
@@ -478,16 +492,15 @@ class Funcionalidad {
     }
 }
 
-
-//Preguntas
-// const preguntas = css.map(pregunta => new Pregunta(pregunta.pregunta, pregunta.opciones, pregunta.respuesta));
-
-
 //logica
+
+
+//para la logica hize una variable que guarda el valor adquirido por un prompt y muestra los temas disponibles que el usuario puede escoger
 
 alert('¿Listo?');
 let tema = parseInt(prompt('Elige un tema tipeando el numero: \n 1. HTML \n 2. CSS \n 3. ARTE \n 4. CIENCIA \n 5. HISTORIA \n 6. GEOGRAFIA \n 7. DEPORTE'));
 
+//este if valida el tema escogido y crea una variable donde manda a llamar el arreglo del tema con su metodo .map que recorre el arreglo y nos devuelve un objeto nuevo (Pregunta)
 if(tema === 1) {
     var preguntas = html.map(pregunta => new Pregunta(pregunta.pregunta, pregunta.opciones, pregunta.respuesta));
 }else if(tema === 2) {
@@ -506,15 +519,20 @@ if(tema === 1) {
 
 }
 
+//ya con la variable preguntas definida por el tema escogido por el usuario, el objeto pregunta ha sido rellenado con los parametros del arreglo escogido por el usuario
+//creamos la funcion principal que se encargara de crear una partida con el tema escogido, esta funcion ejecuta una constante que a su vez crea una clase nueva juego con su parametro preguntas antes declarado
+//despues ejecuta otra constante que a su vez crea una clase funcionalidad
+//luego la funcion renderiza la pagina y muestra solo la puntuacion, esto con la constante renderizarPagina que tiene como funcion condicionar si juego a finalizado con su metodo aFinalizado y ejecuta la puntuacion de dicho juego, si no utiliza el metodo de mostrar pregunta y opciones de la clase funcionalidad para crear una funcion nueva que ejecute la clase juego con su metodo adivina que verificara la respuesta y posterior a esta renderizar en pantalla el resultado.
+
+
 /**
  *
- * @param {Juego} juego objeto priincipal del juego
+ * @param {Juego} juego objeto principal del juego
  * @param {Funcionalidad} funcionalidad objeto de funcionalidad
  */
 const renderizarPagina = (juego, funcionalidad) => {
 
     if(juego.aFinalizado()) {
-        console.log(juego.puntaje)
         funcionalidad.mostrarPuntuacion(juego.puntaje);
     }else {
         funcionalidad.mostrarPregunta(juego.numeroDePregunta().pregunta);
